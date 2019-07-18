862b5c4b8af50062476fe8047513bdd7255ee5e2
t-murata <t-murata@townsystem.co.jp>
2019-07-18 09:31:17 +0900

    リファクタリング
    JSXで利用するBootstrapの記述方式を修正

--- src/components/MenuButtonGroup.jsx
+++ src/components/MenuButtonGroup.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { Row, Container, Col, Button, ButtonToolbar} from 'react-bootstrap';
 import { getEmployeeList, updateEmployeeInfo } from '../actions';
 import store from '../store/configureStore';
 import './MenuButtonGroup.css';
@@ -41,16 +40,16 @@ class MenuButtonGroup extends Component {
   render() {
 
     return (
-      <div className='row'>
-        <div className='container'>
-          <ButtonToolbar>
-            <div className='col-3 menu-button-group'><Button variant='light' className='w-100' onClick={this.close}>終了</Button></div>
-            <div className='col-3 menu-button-group'><Button variant='light' className='w-100' onClick={this.reload}>再読込</Button></div>
-            <div className='col-3 menu-button-group'><Button variant='light' className='w-100' onClick={this.edit_myself}>自分編集</Button></div>
-            <div className='col-3 menu-button-group'><Button variant='light' className='w-100' onClick={this.minimize}>最小化</Button></div>
+      <Row>
+        <Container>
+          <ButtonToolbar className='menu-button-group'>
+            <Col md={3}><Button variant='light' className='w-100' onClick={this.close}>終了</Button></Col>
+            <Col md={3}><Button variant='light' className='w-100' onClick={this.reload}>再読込</Button></Col>
+            <Col md={3}><Button variant='light' className='w-100' onClick={this.edit_myself}>自分編集</Button></Col>
+            <Col md={3}><Button variant='light' className='w-100' onClick={this.minimize}>最小化</Button></Col>
           </ButtonToolbar>
-        </div>
-      </div>
+        </Container>
+      </Row>
     );
   }
 }
