5dd73b4ceb8feb2887e80292f1c343ff88140ad6
takahiro murata <mrt.takahiro@gmail.com>
2019-08-07 00:14:19 +0900

    MacOSでもスタートアップを設定可能なように変更

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -113,8 +113,8 @@ function createWindow() {
 
   createTray();
 
-  // MacOS以外の初回起動時、スタートアップ登録のダイアログを表示する（ダイアログ表示は1度きり）
-  if (!electronStore.get('notified_startup') && process.platform !== 'darwin') {
+  // スタートアップ登録のダイアログを表示する（ダイアログ表示は1度きり）
+  if (!electronStore.get('notified_startup')) {
     const index = electron.dialog.showMessageBox(mainWindow, {
       title: '行き先掲示板',
       type: 'info',
