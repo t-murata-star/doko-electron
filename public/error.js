5f30f30b45943f74f6641fded5f887e544b2d9c3
t-murata <t-murata@townsystem.co.jp>
2019-12-05 13:38:54 +0900

    エラー画面にて、再読み込み中のボタンのテキストを変更

--- public/error.js
+++ public/error.js
@@ -4,5 +4,6 @@ const $ = window.require('jquery');
 const reload = () => {
   // メインプロセスへipc通信を行い、メインプロセスでリロード実行
   $('.reload-btn').prop('disabled', true);
+  $('.reload-btn').val('読み込み中...');
   ipcRenderer.send('reload');
 };
