d26d5eb34ae96a56affc9b46a3f50310210b85e7
t-murata <t-murata@townsystem.co.jp>
2019-12-05 14:48:49 +0900

    未使用のインポートを削除(serviceWorker)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
+import { StylesProvider, ThemeProvider as MaterialThemeProvider } from '@material-ui/styles';
 import React from 'react';
-import './index.css';
-import AppPanel from './containers/AppPanel';
-import * as serviceWorker from './serviceWorker';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/configureStore';
-import { ThemeProvider as MaterialThemeProvider, StylesProvider } from '@material-ui/styles';
 import { baseTheme } from './components/materialui/theme';
+import AppPanel from './containers/AppPanel';
+import './index.css';
+import store from './store/configureStore';
 
 render(
   <StylesProvider injectFirst>
@@ -22,4 +21,3 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
