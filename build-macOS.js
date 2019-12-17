cf4a86f371e3cf0a05c8c3bace9d190cc19525eb
takahiro murata <mrt.takahiro@gmail.com>
2019-12-18 02:16:54 +0900

    MacOSにおけるElectronのビルド形式を変更

--- build-macOS.js
+++ build-macOS.js
@@ -1,24 +1,25 @@
-"use strict"
+'use strict';
 
-const builder = require("electron-builder")
-const Platform = builder.Platform
+const builder = require('electron-builder');
+const Platform = builder.Platform;
 
 // Promise is returned
-builder.build({
-  targets: Platform.MAC.createTarget(),
-  config: {
-    'productName': '行き先掲示板',
-    'appId': `com.electron.s`,
-    'mac': {
-      'target': {
-        'target': 'dmg',
+builder
+  .build({
+    targets: Platform.MAC.createTarget(),
+    config: {
+      productName: '行き先掲示板',
+      appId: `com.electron.s`,
+      mac: {
+        target: {
+          target: 'pkg'
+        }
       }
     }
-  }
-})
+  })
   .then(() => {
     // handle result
   })
-  .catch((error) => {
+  .catch(error => {
     // handle error
-  })
+  });
