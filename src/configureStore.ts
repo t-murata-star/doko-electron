610def85451b2e06ab13c0afe90f20e652e861a1
t-murata <t-murata@townsystem.co.jp>
2020-01-28 16:28:09 +0900

    本番環境にてredux-loggerが動作しないように変更

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,9 +1,14 @@
 import { applyMiddleware, createStore, Store } from 'redux';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer, { RootState } from './modules';
 
-const logger = createLogger();
-const middlewares = [thunk, logger];
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { createLogger } = window.require('redux-logger');
+  const logger = createLogger();
+  middlewares.push(logger);
+}
+
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export default store as Store<RootState>;
