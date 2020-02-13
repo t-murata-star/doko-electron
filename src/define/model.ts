e9ce2c35d8e73e433ae25baaaed24f856d4fa11a
t-murata <mrt.takahiro@gmail.com>
2020-02-13 21:46:16 +0900

    ユーザ一覧画面における、「在席」状態の文字色を変更

--- src/define/model.ts
+++ src/define/model.ts
@@ -40,7 +40,7 @@ export class UserInfo {
 }
 
 export class UserStatusInfo {
-  s01 = { status: '在席', color: '#000000' };
+  s01 = { status: '在席', color: '#333333' };
   s02 = { status: '退社', color: '#0000FF' };
   s03 = { status: '年休', color: '#FF0000' };
   s04 = { status: 'AM半休', color: '#00A900' };
@@ -52,7 +52,7 @@ export class UserStatusInfo {
   s10 = { status: '遅刻', color: '#00A900' };
   s11 = { status: '行方不明', color: '#FF0000' };
   s12 = { status: '接客中', color: '#00A900' };
-  s13 = { status: '在席 (離席中)', color: '#000000' };
+  s13 = { status: '在席 (離席中)', color: '#333333' };
 }
 
 export class Notification {
